5d930cb2803e4d00dda37c98cc540b1e5e62072f
amitgupta1000 <37208475+amitgupta1000@users.noreply.github.com>
2025-11-16 15:33:48 +0530

    Change uvicorn.run to use 'main:app' syntax

--- main.py
+++ main.py
@@ -315,4 +315,4 @@ if __name__ == "__main__":
 
     port = int(os.getenv("PORT", 8000))
     reload = os.getenv("ENVIRONMENT") != "production"
-    uvicorn.run(app, host="0.0.0.0", port=port, reload=reload, loop="asyncio")
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=reload, loop="asyncio")
